9ee3081fa3acefadf0ff57861778ac97b8ea7eb5
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-11 22:27:19 -0400

    Recibir y validar la data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreatePokemonDto } from './pokemon/dto/create-pokemon.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createPokemon( @Body() createPokemonDto: CreatePokemonDto ) {
+    return this.appService.create( createPokemonDto );
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePokemonDto } from './pokemon/dto/create-pokemon.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  create( createPokemonDto: CreatePokemonDto ) {
+    return createPokemonDto;
+  }
+
 }
