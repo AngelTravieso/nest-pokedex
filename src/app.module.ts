be54bf08d890a006c169b8afb68191f0d8b7f17b
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-11 21:47:30 -0400

    Conectar Nest con Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,21 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    // Configuración para carpeta static/public
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
+
+
+      MongooseModule.forRoot(
+        // URI de la BD de mongo
+        'mongodb://localhost:27017/nest-pokemon'
+      ),
+
     PokemonModule,
   ],
 })
