de32e2380b5a39ba66cfdf527ab5158602515692
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-17 21:19:41 -0400

    Insertar Pokemons por lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { CreatePokemonDto } from 'src/pokemon/dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+  
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel = Model<Pokemon>,
+  ) {}
   
   async executeSeed() {
 
@@ -14,17 +23,19 @@ export class SeedService {
     // Tipar respuesta con la interface PokeResponse
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach( async ({ name, url }) => {
       // console.log({name, url});
 
       // Split para buscar el N° (id) del pokemon
       const segments = url.split('/')
       const no = +segments[segments.length - 2]; // Obtener ID Pokemon
-      console.log({ name, no });
+      // console.log({ name, no });
+
+      await this.pokemonModel.create({ no, name });
 
     });
 
-    return data.results;
+    return 'SEED EXECUTED';
 
   }
 
